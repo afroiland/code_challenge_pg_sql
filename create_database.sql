437007e8858917ba871fe3b30b3fb60bbf60a49c
Andrew Froiland <afroiland@Andrews-MacBook-Pro-2.local>
2016-11-21 14:04:22 -0600

    base mode complete

--- create_database.sql
+++ create_database.sql
@@ -1,3 +1,13 @@
 -- Database name
 
 -- Document your create tables SQL here
+CREATE TABLE treats (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	description VARCHAR(150) NOT NULL,
+	pic VARCHAR(300) NOT NULL
+);
+
+INSERT INTO treats (name, description, pic)
+VALUES ('Cupcake', 'A delicious cupcake', '/assets/cupcake.jpg'),
+('Donuts', 'Mmmm donuts', '/assets/donuts.jpg');
